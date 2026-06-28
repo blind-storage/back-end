8e5f2f97340cdf655595b89d5890e3e1ffc36b76
Amine Slaoui <amine.slaoui@telecom-paris.fr>
2026-06-28 22:16:57 +0200

    refactor(migration): remove unused signing keys from User table migration

--- prisma/migrations/20260628152520_add_signing_keys/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "key_certificate" JSONB,
-ADD COLUMN     "key_certificate_signature" TEXT,
-ADD COLUMN     "key_fingerprint" TEXT,
-ADD COLUMN     "sign_priv_key_enc_1" TEXT,
-ADD COLUMN     "sign_priv_key_enc_2" TEXT,
-ADD COLUMN     "sign_pub_key" TEXT;
